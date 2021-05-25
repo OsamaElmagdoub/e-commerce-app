7ad77731e9ecf929ef973c6a7753fa588606ae24
OsamaElmagdoub <71664948+OsamaElmagdoub@users.noreply.github.com>
2021-05-25 23:00:47 +0200

    Update signup_screen.dart

--- lib/screens/user/signup_screen.dart
+++ lib/screens/user/signup_screen.dart
@@ -5,6 +5,7 @@ import 'package:ecommerce/services/auth.dart';
 import 'package:flutter/material.dart';
 import 'package:modal_progress_hud/modal_progress_hud.dart';
 import 'package:provider/provider.dart';
+import '../../widgets/custom_texfield.dart';
 
 class SignUpScreen extends StatelessWidget {
   static String id = 'signupscreen';
